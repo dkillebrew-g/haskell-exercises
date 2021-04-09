c17c4efd4f43254dfe18f71efd5f45a6dbb42a29
Dan K <n/a>
2021-04-09 14:17:21 -0700

    notes from class

--- src/Main.hs
+++ src/Main.hs
@@ -19,13 +19,17 @@ type IntToString = Map Int String
 
 -- Reader is a function like this: (someData -> a)
 
-newtype MyFunctionContainer someData return = MyFunctionContainer (someData -> return)
+newtype MyReader someData return = MyReader (someData -> return)
 
--- Functor
--- fmap :: (a -> b) -> Reader someData a -> Reader someData b
+instance Functor  (MyReader someData) where
+  fmap :: (a -> b) -> MyReader someData a -> MyReader someData b
+  fmap = undefined
 
--- Applicative
--- pure :: a -> Reader someData a
+instance Applicative (MyReader someData) where
+  pure :: a -> MyReader someData a
+  pure somevalue = MyReader (\_ -> somevalue)
+  -- or equivalently:
+  -- pure somevalue = MyReader (const somevalue)
 -- (<*>) :: Reader someData (a -> b) -> Reader someData a -> Reader someData b
 
 -- Monad
@@ -53,11 +57,23 @@ type MapReader = Reader IntToString
 
 -- Does the same as the version above, but this time using the Reader type.
 lookupIntR :: Int -> MapReader String
-lookupIntR = undefined
+lookupIntR i =
+  asks (\mp -> lookupInt mp i)
+  -- do
+  -- mp <- ask
+  -- pure (lookupInt mp i)
 
 -- Does the same as the version above, but this time using the Reader type.
 lookupTwoIntsR :: (Int, Int) -> MapReader (String, String)
-lookupTwoIntsR = undefined
+lookupTwoIntsR (i0, i1) = do
+  s0 <- lookupIntR i0
+  s1 <- lookupIntR i1
+  pure (s0, s1)
+-- lookupTwoIntsR (i0, i1) =
+--   liftM2 (,) (lookupIntR i0) (lookupIntR i1)
+
+lookupTwoIntsList :: [Int] -> MapReader [String]
+lookupTwoIntsList = mapM lookupIntR
 
 -- If the Int key is not in the map, add it to the map with value "unknown",
 -- then do the lookup.
