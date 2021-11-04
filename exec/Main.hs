e5c43e7d20baf6d4f3f5c3da786189cbb9cf9c2c
Dan K <n/a>
2021-11-04 11:55:58 -0700

    Begin mtl exercise

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,9 +1,14 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main (main) where
 
 import ClassyPrelude
-import Control.Monad.Trans.Maybe
+
+import Control.Monad.Except
+import Control.Monad.Reader
+import Control.Monad.Writer
+
 
 -- MTL (Monad transformer library)?
 
@@ -28,22 +33,15 @@ getLineJustIo = getLine
 printJustIo :: Show a => a -> IO ()
 printJustIo = print
 
--- The monad transformer you will use will be `MaybeT IO a`
--- Do all of these in one `do` block:
-
--- read a value from the console (getLine)
 -- parse it to an integer (could fail)
 -- div2
 -- mul2
--- print it back out (only if it parsed AND divided by 2 successfully)
 -- return the result of mul2
-
--- lift :: (MonadTrans t, Monad m) => m a -> t m a
-
--- transformers	Control.Monad.Trans.Class
--- Lift a computation from the argument monad to the constructed monad.
-
--- readMay :: Read a => Text -> Maybe a
+parseAndDiv :: (MonadError () m, MonadReader Text m) => m Int
+parseAndDiv = do
+   line <- ask
+   _ <- when undefined (throwError ())
+   pure undefined
 
 main :: IO ()
 main = do
@@ -57,10 +55,7 @@ main = do
       Nothing -> pure ()
       Just x -> print x
 
-  -- Now do things using the MaybeT monad transformer.
-  -- the entire expression to the right of the `<-` must be of type `MaybeT IO a`
-  let readAndDivMulPrint :: MaybeT IO Int
-      readAndDivMulPrint = do
-        pure undefined
+  -- We'll call parseAndDiv, passing it the line we got via getLine
+  -- if it succeeds and returns an int, then print the int.
 
   pure ()
