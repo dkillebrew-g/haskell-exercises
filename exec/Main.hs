33bd3d46894370454524e3965e0a7c4a2090f294
Dan K <n/a>
2021-11-04 11:18:35 -0700

    One way to do it in IO.

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main (main) where
 
 import ClassyPrelude
@@ -11,12 +13,20 @@ import Control.Monad.Trans.Maybe
 -- Do a stackage search for MaybeT
 -- skim/read the docs
 
+-- this is in the branch 'exercise/monad_transformer':
+
 -- Returns (Just x/2) when x is evenly divisible by 2.
 div2 :: Int -> Maybe Int
 div2 x = undefined
 
 mul2 :: Int -> Int
 mul2 = (* 2)
+
+getLineJustIo :: IO Text
+getLineJustIo = getLine
+
+printJustIo :: Show a => a -> IO ()
+printJustIo = print
 
 -- The monad transformer you will use will be `MaybeT IO a`
 -- Do all of these in one `do` block:
@@ -25,9 +35,32 @@ mul2 = (* 2)
 -- parse it to an integer (could fail)
 -- div2
 -- mul2
--- print it back out
+-- print it back out (only if it parsed AND divided by 2 successfully)
 -- return the result of mul2
+
+-- lift :: (MonadTrans t, Monad m) => m a -> t m a
+
+-- transformers	Control.Monad.Trans.Class
+-- Lift a computation from the argument monad to the constructed monad.
+
+-- readMay :: Read a => Text -> Maybe a
 
 main :: IO ()
 main = do
-  putStrLn "Hello world"
+  line <- getLine
+  let asInt :: Maybe Int
+      asInt = readMay line
+      divided_fmap = join (fmap div2 asInt) -- just for demonstration
+      divided = asInt >>= div2
+      multiplied = fmap mul2 divided
+  case multiplied of
+      Nothing -> pure ()
+      Just x -> print x
+
+  -- Now do things using the MaybeT monad transformer.
+  -- the entire expression to the right of the `<-` must be of type `MaybeT IO a`
+  let readAndDivMulPrint :: MaybeT IO Int
+      readAndDivMulPrint = do
+        pure undefined
+
+  pure ()
