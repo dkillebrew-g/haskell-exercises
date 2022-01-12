f252e5ceb6f3e3b97b849657fd5bc0e8e533dcba
Dan K <n/a>
2022-01-12 14:45:11 -0800

    Finish Introduction

--- src/LispVal.hs
+++ src/LispVal.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module LispVal where
 
 import ClassyPrelude
-
+import Control.Monad.Except
+import Control.Monad.Reader
 
 -- | All kinds of Lisp values
 data LispVal
@@ -17,6 +20,31 @@ data LispVal
   | Bool Bool
   deriving (Typeable)
 
-data IFunc
+data IFunc = IFunc {fn :: [LispVal] -> Eval LispVal}
 
-data EnvCtx
+type EnvCtx = Map Text LispVal
+
+newtype Eval a = Eval {unEval :: ReaderT EnvCtx IO a}
+  deriving
+    ( Monad,
+      Functor,
+      Applicative,
+      MonadReader EnvCtx,
+      MonadIO
+    )
+
+instance Show LispVal where
+  show = unpack . showVal
+
+showVal :: LispVal -> Text
+showVal val =
+  case val of
+    (Atom atom) -> atom
+    (String str) -> concat ["\"", str, "\""]
+    (Number num) -> pack $ show num
+    (Bool True) -> "#t"
+    (Bool False) -> "#f"
+    Nil -> "Nil"
+    (List contents) -> concat ["(", unwords $ showVal <$> contents, ")"]
+    (Fun _) -> "(internal function)"
+    (Lambda _ _) -> "(lambda function)"
