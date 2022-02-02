006caa8c99e5d9335aa1293e9b6692b78f69b3ab
Dan K <n/a>
2022-02-02 15:18:54 -0800

    impl some parsers

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,10 +1,10 @@
 module Parser where
 
-import ClassyPrelude
+import ClassyPrelude hiding (many, some)
 import Data.Void (Void)
 import LispVal
 import Text.Megaparsec
-import Text.Megaparsec.Char (space1)
+import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void Text
@@ -25,3 +25,32 @@ lexeme = L.lexeme sc
 -- internally) and then consumes all trailing white space using `sc`.
 symbol :: Text -> Parser Text
 symbol = L.symbol sc
+
+identifier :: Parser Text
+identifier =
+  let start :: Parser Char
+      start = letterChar <|> oneOf ['-', '+', '/', '*', '=', '|', '&', '>', '<']
+      tailChar :: Parser Char
+      tailChar = digitChar <|> start
+      unpacked :: Parser [Char]
+      unpacked = (:) <$> start <*> many tailChar
+   in pack <$> unpacked
+
+parseAtom :: Parser LispVal
+parseAtom = lexeme (Atom <$> identifier)
+
+parseText :: Parser LispVal
+parseText = do
+  p <- char '"' *> takeWhile1P (Just "Text") (/= '"') <* char '"'
+  lexeme (pure (String p))
+
+fromJust :: Maybe a -> a
+fromJust Nothing = error "fromJust"
+fromJust (Just x) = x
+
+parseNumber :: Parser LispVal
+parseNumber = do
+  negativeMay <- optional (char '-')
+  digits <- some digitChar
+  let signedDigits = maybe digits (: digits) negativeMay
+  lexeme . pure . Number . fromJust . readMay $ signedDigits
